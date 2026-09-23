67c3257bd9e4317ff9ee2eded50bafdb5fa2aac8
marscancode <marsadesina@gmail.com>
2026-09-23 20:34:29 +0100

    added prediction and explanation of code

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,10 @@
 // Predict and explain first...
-//  =============> write your prediction here
-
+//This code will throw an error as we haven't passed an argument into str? or str is the function parameter and variable already on line 5 so it may throw an error to declare again  as a variable inside the function?
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
-
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
 // =============> write your explanation here
 // =============> write your new code here
