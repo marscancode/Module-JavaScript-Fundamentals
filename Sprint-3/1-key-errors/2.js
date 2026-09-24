82a32efb506f3e157b11b85d9ae84a67b17cffb6
marscancode <marsadesina@gmail.com>
2026-09-24 16:28:12 +0100

    added prediction and explanation of current code

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -3,7 +3,7 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// I think this code will throw an error because we are passing an actual number into the parameters but javascript only allows a variable to be passed into the parameters that can be used to store information so i would need to change the number 3 into num ( and num here would act as the placeholder for any number passed in when the function gets called later) for this code to work because as it is i believe num would be undefined as it hasn't been defined anywhere.
 
 function square(3) {
     return num * num;
