34de47bd8672b6be18b856948d615751b6c6473b
marscancode <marsadesina@gmail.com>
2026-09-25 13:15:33 +0100

    finished cases exercise

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -14,3 +14,13 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+// define function
+function stringToUpperCase(str) {
+  //store string in upper snake case
+  let stringToUpperCaseResult = str.toUpperCase().replaceAll(" ", "_");
+  // return result
+  return stringToUpperCaseResult;
+}
+// call function
+console.log(stringToUpperCase("welcome to cyf"));
