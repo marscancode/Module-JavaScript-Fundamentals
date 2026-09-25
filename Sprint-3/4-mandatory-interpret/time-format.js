c4ba0d997958766fba3afc7dc25e7af55c831c26
marscancode <marsadesina@gmail.com>
2026-09-25 23:47:24 +0100

    finished time-format exercises

--- Sprint-3/4-mandatory-interpret/time-format.js
+++ Sprint-3/4-mandatory-interpret/time-format.js
@@ -15,24 +15,26 @@ function formatTimeDisplay(seconds) {
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
+console.log(formatTimeDisplay(61));
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+//  Pad will be called 3 times which is seen on line 15
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// 0
 
 // c) What is the return value of pad when it is called for the first time?
-// =============> write your answer here
+// the return value of pad when called for the first time is 00
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// 1 second is stored in remainingSeconds so the value 1 is assigned to num when pad is called for the last time.
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// the return value is 01 as the variable numString stores "01"
